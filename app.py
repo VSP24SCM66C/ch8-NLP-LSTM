5e7a989bd248e1b8185ba80afafdd72e9689012c
cvs1 <cvssivateja@gmail.com>
2024-04-19 15:32:46 -0500

    file to 2.0

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ print (my_sentence2, ' : ', predict_sentiment(my_sentence2))
 
 @app.route("/")
 def entry():
-    return "Wellcome!"
+    return "Wellcome! 2.0"
     
 # build a route or HTTP endpoint
 @app.route('/hello')
